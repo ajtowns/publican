21060971ce31bf37b12f84e7298101aeb5cbaef4
Jeff Fearn <jfearn@redhat.com>
2008-09-09 05:04:16 +0000

    fix test script
    git-svn-id: file:///srv/svn/publican/trunk@487 322d0b13-9966-4b26-b068-8724d74359d3

--- publican/test-create_book.sh
+++ publican/test-create_book.sh
@@ -8,12 +8,12 @@ pwd=`pwd`
 #for type in Book Article Set; do
 for type in Book Article; do
 	for brand in common fedora JBoss RedHat; do
-		create_book --name=Test-$brand-$type --brand=$brand --type=$type;
-#		make --directory=Test-$brand-$type html-en-US html-single-en-US pdf-en-US;
-		make --directory=Test-$brand-$type html-single-en-US;
+		create_book --name=Test-$brand-$type --brand=$brand --type=$type --product=$brand;
+		make --directory=Test-$brand-$type html-en-US html-single-en-US pdf-en-US;
+#		make --directory=Test-$brand-$type html-single-en-US;
 		(firefox $pwd/Test-$brand-$type/tmp/en-US/html-single/index.html &);
-#		(firefox Test-$brand-$type/tmp/en-US/html/index.html&)
-#		(evince Test-$brand-$type/tmp/en-US/pdf/Test-$brand.pdf &)
+		(firefox Test-$brand-$type/tmp/en-US/html/index.html&)
+		(evince Test-$brand-$type/tmp/en-US/pdf/Test-$brand-$type.pdf &)
 	done
 done
 
