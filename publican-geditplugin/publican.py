6b2d6319377b7ee81b9cd205777630285abeddf9
ryan lerch <rlerch@redhat.com>
2009-03-27 03:26:07 +0000

    added xiincludes tool
    git-svn-id: file:///srv/svn/publican/trunk@695 322d0b13-9966-4b26-b068-8724d74359d3

--- publican-geditplugin/publican.py
+++ publican-geditplugin/publican.py
@@ -8,6 +8,9 @@ ui_str = """<ui>
     <menu name="Publican" action="PublicanMenuAction">
         <menuitem name="OpenDocument" action="OpenDocument"/>
         <menuitem name="CreateDocument" action="CreateDocument"/>
+        <menu name="PublicanMenuTools" action="PublicanMenuTools">
+            <menuitem name="xiincludetool" action="xiincludetool"/>
+        </menu>
     </menu>
   </menubar>
 </ui>
@@ -70,7 +73,7 @@ class ResultsView(gtk.VBox):
         self.pack_start(scrolled_window)
         self.show_all()
         
-
+        
         filename = path.rpartition('/')[2]+'.xml'
         bookpath = path
         path = path+'/'+self.DEFAULT_LANG+'/'
@@ -141,6 +144,8 @@ class PluginHelper:
         self._action_group.add_actions([("OpenDocument", None, _("Open Document"), None, _("Open an Existing Publican Document"), self.on_open_document_activate)])
         self._action_group.add_actions([("CreateDocument", None, _("Create New Document"), None, _("Create a new publican document"), self.on_create_document_activate)])
         self._action_group.add_actions([("PublicanMenuAction", None, _("Publican"), None, _("Clear the document"), None)])
+        self._action_group.add_actions([("PublicanMenuTools", None, _("Tools"), None, _("Clear the document"), None)])
+        self._action_group.add_actions([("xiincludetool", None, _("xi:include"), "<control><shift>x", _("Clear the document"), self.xiinclude_tool)])
         manager.insert_action_group(self._action_group, -1)
         self._ui_id = manager.add_ui_from_string(ui_str)
 
@@ -177,7 +182,30 @@ class PluginHelper:
         image.set_from_stock(gtk.STOCK_DND_MULTIPLE, gtk.ICON_SIZE_BUTTON)
         self.ui_id = panel.add_item(self.results_view, "Publican Document View", image)
         panel.activate_item(self.results_view)
-
+    def xiinclude_tool(self, action):
+        doc = self.window.get_active_document()
+        if not doc:
+            return
+        text = doc.get_text(doc.get_selection_bounds()[0], doc.get_selection_bounds()[1], False)
+        filepath = doc.get_uri_for_display().rpartition("/")[0]
+        dialog = gtk.Dialog("My dialog", None, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        entry = gtk.Entry()
+        box = dialog.vbox
+        box.add(entry)
+        box.show_all()
+        dialog.run()
+        thefilename = entry.get_text()
+        dialog.destroy()
+        node = minidom.parseString(text)
+        f = open(filepath+'/'+thefilename,'w')
+        f.write("<?xml version='1.0'?>\n")
+        f.write("<!DOCTYPE "+node.documentElement.tagName+" PUBLIC \"-//OASIS//DTD DocBook XML V4.5//EN\" \"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd\" [\n")
+        f.write("]>\n\n")
+        f.write(text)
+        f.close()
+        self.window.create_tab_from_uri("file://"+filepath+'/'+thefilename, None, 1, False, True)
+        doc.delete_selection(True, True)
+        doc.insert_at_cursor("<xi:include href=\""+thefilename+"\" xmlns:xi=\"http://www.w3.org/2001/XInclude\" />")
 
 class PublicanPlugin(gedit.Plugin):
     def __init__(self):
