94d79fae8ecc97b03b602c114658be24e864c1fb
Jeff Fearn <jfearn@redhat.com>
2009-12-02 05:53:34 +0000

    update pot
    git-svn-id: file:///srv/svn/publican/trunk@903 322d0b13-9966-4b26-b068-8724d74359d3

--- publican/lib/Publican.pm
+++ publican/lib/Publican.pm
@@ -17,7 +17,7 @@ use Publican::Localise;
 
 use vars qw(@ISA $VERSION @EXPORT @EXPORT_OK $SINGLETON $LOCALISE);
 
-$VERSION = version->new('1.0');
+$VERSION = version->new('1.3');
 @ISA     = qw(Exporter AutoLoader);
 
 @EXPORT
