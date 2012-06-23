d60b0f1d2a417ed15dfb4da6d27b946f5b7be7ac
Jeff Fearn <Jeff.Fearn@gmail.com>
2012-06-23 11:02:36 +1000

    Fix autor tests failing, enable tests in rpm build. BZ #747871

--- publican/lib/Publican/WebSite.pm
+++ publican/lib/Publican/WebSite.pm
@@ -2041,81 +2041,13 @@ Writes a javascript file with an associative array of labels.
 
 Migrate a website DataBase from Publican < 3 to Publican 3.
 
-=head1 DIAGNOSTICS
+=head2 insensitive_sort
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+Sort strings in a case insensitive order.
 
-=over
+=head2 version_sort
 
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-Publican::WebSite requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-documentation-website@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
+Sort version strings in correct order. Handles X Vs X.Y Vs X.Y.Z.
 
 =head1 AUTHOR
 
